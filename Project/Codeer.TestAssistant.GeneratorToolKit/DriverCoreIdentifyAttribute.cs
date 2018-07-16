a426060b253c65a602da49b89a486b28efac1ef8
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2018-07-16 14:55:17 +0900

    Add DruverCoreIdentifyAttribute.

---
+++ Project/Codeer.TestAssistant.GeneratorToolKit/DriverCoreIdentifyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Codeer.TestAssistant.GeneratorToolKit
+{
+    /// <summary>
+    /// Attributes for specifying Driver's core.
+    /// Core can specify WindowControl, IAppVarOwner, AppVar, DynamicAppVar.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DriverCoreIdentifyAttribute : Attribute { }
+}
