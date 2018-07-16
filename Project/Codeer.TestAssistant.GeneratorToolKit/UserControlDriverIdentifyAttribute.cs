30e23f2de7523697776b5e6f7d91d9e4617a37fd
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2018-07-16 13:15:30 +0900

    Maintenance UserControlIdentifyAttribute

--- Project/Codeer.TestAssistant.GeneratorToolKit/UserControlDriverIdentifyAttribute.cs
+++ Project/Codeer.TestAssistant.GeneratorToolKit/UserControlDriverIdentifyAttribute.cs
@@ -9,26 +9,11 @@ namespace Codeer.TestAssistant.GeneratorToolKit
     public class UserControlDriverIdentifyAttribute : Attribute
     {
         /// <summary>
-        /// Window class.
-        /// </summary>
-        public string WindowClass { get; set; }
-
-        /// <summary>
-        /// .Net type full name.
-        /// </summary>
-        public string TypeFullName { get; set; }
-
-        /// <summary>
-        /// Window text.
-        /// </summary>
-        public string WindowText { get; set; }
-
-        /// <summary>
         /// Name of the method that supports UserControl identification when generating capture code.
         /// It must be defined in the same class.
         /// The type is next.
         /// static UserControlDriver[] CustomFunction(WindowDriver window, out T[] identifier);
         /// </summary>
-        public string CustomMethodName { get; set; }
+        public string CustomMethod { get; set; }
     }
 }
