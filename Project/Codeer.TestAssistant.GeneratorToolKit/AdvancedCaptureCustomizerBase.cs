04060421f848815f9dc9078dbb125096a827c327
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2018-08-22 15:09:29 +0900

    Adjust AdvancedCaptureCustomizer.

--- Project/Codeer.TestAssistant.GeneratorToolKit/AdvancedCaptureCustomizerBase.cs
+++ Project/Codeer.TestAssistant.GeneratorToolKit/AdvancedCaptureCustomizerBase.cs
@@ -5,12 +5,16 @@ namespace Codeer.TestAssistant.GeneratorToolKit
     /// <summary>
     /// Advanced capture customization function.
     /// </summary>
-    public abstract class AdvancedCaptureCustomizer
+    public abstract class AdvancedCaptureCustomizerBase
     {
+        internal delegate void AttachCaptoreGeneratorDelegate(string accessPath, object driver);
+        internal delegate void DeclearVariableDelegate(string name, string initialize, object driver);
         internal delegate VisualItemState GetVisualItemStateDelegate(object driver);
         internal delegate AttachMethodInvokeInfo[] GetTopLevelWindowAttacDriverInfoDelegate(IntPtr handle);
         internal delegate AttachMethodInvokeInfo[] GetAttacDriverInfoDelegate(object parentDriver);
 
+        internal AttachCaptoreGeneratorDelegate AttachCaptoreGeneratorCore { get; set; }
+        internal DeclearVariableDelegate DeclearVariableCore { get; set; }
         internal GetVisualItemStateDelegate GetVisualItemStateCore { get; set; }
         internal GetTopLevelWindowAttacDriverInfoDelegate GetTopLevelWindowAttacDriverInfoCore { get; set; }
         internal GetAttacDriverInfoDelegate GetAttacDriverInfoCore { get; set; }
@@ -35,6 +39,21 @@ namespace Codeer.TestAssistant.GeneratorToolKit
         /// <param name="parentDriver">Parent driver.</param>
         /// <returns>Attachment information.</returns>
         protected AttachMethodInvokeInfo[] GetAttacDriverInfo(object parentDriver) => GetAttacDriverInfoCore(parentDriver);
+
+        /// <summary>
+        /// Declear variable.
+        /// </summary>
+        /// <param name="name">Variable name.</param>
+        /// <param name="initialize">Initialize code.</param>
+        /// <param name="driver">Driver.</param>
+        protected void DeclearVariable(string name, string initialize, object driver) => DeclearVariableCore(name, initialize, driver);
+
+        /// <summary>
+        /// Create and attach a generator for capture code generation.
+        /// </summary>
+        /// <param name="accessPath">Access path to ControlDriver in generated code.</param>
+        /// <param name="controlDriver">Control Driver</param>
+        protected void AttachCaptoreGenerator(string accessPath, object controlDriver) => AttachCaptoreGeneratorCore(accessPath, controlDriver);
 
         /// <summary>
         /// Initialize.
