95c3e6e37a444f55f379bee8ffdfdec589dcfcc4
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2018-08-23 22:41:54 +0900

    Add advanced customize option.

---
+++ Project/Codeer.TestAssistant.GeneratorToolKit/AdvancedCreateDriverCustomizerBase.cs
@@ -0,0 +1,18 @@
+﻿namespace Codeer.TestAssistant.GeneratorToolKit
+{
+    /// <summary>
+    /// Advanced customization of driver creation.
+    /// </summary>
+    public abstract class AdvancedCreateDriverCustomizerBase
+    {
+        /// <summary>
+        /// Create code.
+        /// </summary>
+        /// <param name="targetObj">Target object.</param>
+        /// <param name="nameSpace">namespace.</param>
+        /// <param name="className">class name.</param>
+        /// <param name="code">code.</param>
+        /// <returns></returns>
+        public abstract bool Create(object targetObj, string nameSpace, out string className, out string code);
+    }
+}
---
+++ Project/Codeer.TestAssistant.GeneratorToolKit/AdvancedExecuteCustomizerBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Codeer.TestAssistant.GeneratorToolKit
+{
+    /// <summary>
+    /// Advanced customization of process execution.
+    /// </summary>
+    public abstract class AdvancedExecuteCustomizerBase
+    {
+        /// <summary>
+        /// Initialization of class to execute processing.
+        /// </summary>
+        /// <param name="target">Target.</param>
+        /// <param name="appFriend">AppFriend.</param>
+        public abstract void Initialize(object target, object appFriend);
+    }
+}
