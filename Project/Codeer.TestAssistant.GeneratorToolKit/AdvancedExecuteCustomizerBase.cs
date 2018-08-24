679b7ee27fb36eda3f0df6a630aa824877e58051
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2018-08-24 13:01:08 +0900

    Change specific Advanced customize.

--- Project/Codeer.TestAssistant.GeneratorToolKit/AdvancedExecuteCustomizerBase.cs
+++ Project/Codeer.TestAssistant.GeneratorToolKit/AdvancedExecuteCustomizerBase.cs
@@ -9,7 +9,8 @@
         /// Initialization of class to execute processing.
         /// </summary>
         /// <param name="target">Target.</param>
+        /// <param name="methodName">Method name.</param>
         /// <param name="appFriend">AppFriend.</param>
-        public abstract void Initialize(object target, object appFriend);
+        public abstract void Initialize(object target, string methodName, object appFriend);
     }
 }
