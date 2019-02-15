c5b8dc95f3f242fe1039c614dad05ded479a35a8
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2019-02-15 19:01:09 +0900

    Fix typo.

---
+++ Project/Codeer.TestAssistant.GeneratorToolKit/ICaptureAttachTreeMenuAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codeer.TestAssistant.GeneratorToolKit
+{
+    /// <summary>
+    /// Menu processing at Capture Attach Tree.
+    /// </summary>
+    public interface ICaptureAttachTreeMenuAction
+    {
+        /// <summary>
+        /// Get Menu processing at Capture Attach Tree.
+        /// </summary>
+        /// <param name="accessPath">Access path to driver.</param>
+        /// <param name="driver">Window driver or UserControl driver or Control driver.</param>
+        /// <returns>Menu processing at Capture Attach Tree.</returns>
+        Dictionary<string, MenuAction> GetAction(string accessPath, object driver);
+    }
+
+    /// <summary>
+    /// Obsolete.
+    /// </summary>
+    [Obsolete("Sorry this class name is typo. ：_(  Please use ICaptureAttachTreeMenuAction.", false)]
+    public interface ICapterAttachTreeMenuAction
+    {
+        /// <summary>
+        /// Obsolete.
+        /// </summary>
+        /// <param name="accessPath">Obsolete.</param>
+        /// <param name="driver">Obsolete.</param>
+        /// <returns>Obsolete.</returns>
+        Dictionary<string, MenuAction> GetAction(string accessPath, object driver);
+    }
+}
