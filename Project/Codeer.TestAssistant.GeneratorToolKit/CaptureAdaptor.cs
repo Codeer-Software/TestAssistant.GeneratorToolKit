5cf61ae258206551182d88cf55d2419cb8937415
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2019-06-17 11:59:24 +0900

    Add functions.
    Add TestAssistantMode.
    Added "Using" in menu processing of CaptureTree.

--- Project/Codeer.TestAssistant.GeneratorToolKit/CaptureAdaptor.cs
+++ Project/Codeer.TestAssistant.GeneratorToolKit/CaptureAdaptor.cs
@@ -8,6 +8,7 @@ namespace Codeer.TestAssistant.GeneratorToolKit
     public static class CaptureAdaptor
     {
         static List<string> _code = new List<string>();
+        static List<string> _using = new List<string>();
 
         /// <summary>
         /// Add code.
@@ -22,10 +23,36 @@ namespace Codeer.TestAssistant.GeneratorToolKit
         }
 
         /// <summary>
+        /// Add using.
+        /// </summary>
+        /// <param name="using">using.</param>
+        public static void AddUsing(string @using)
+        {
+            lock (_using)
+            {
+                _using.Add(@using);
+            }
+        }
+
+        /// <summary>
         /// Get code.
         /// </summary>
         /// <returns>code.</returns>
         internal static string[] PopCode()
+        {
+            lock (_code)
+            {
+                var ret = _code.ToArray();
+                _code.Clear();
+                return ret;
+            }
+        }
+
+        /// <summary>
+        /// Get using.
+        /// </summary>
+        /// <returns>using.</returns>
+        internal static string[] PopUsing()
         {
             lock (_code)
             {
---
+++ Project/Codeer.TestAssistant.GeneratorToolKit/TestAssistantMode.cs
@@ -0,0 +1,44 @@
+﻿namespace Codeer.TestAssistant.GeneratorToolKit
+{
+    /// <summary>
+    /// Test Assistant Pro Mode.
+    /// </summary>
+    public static class TestAssistantMode
+    {
+        /// <summary>
+        /// Mode
+        /// </summary>
+        public enum Mode
+        {
+            /// <summary>
+            /// Non.
+            /// </summary>
+            Non,
+
+            /// <summary>
+            /// Analyze.
+            /// </summary>
+            Analyze,
+
+            /// <summary>
+            /// Capture.
+            /// </summary>
+            Capture,
+
+            /// <summary>
+            /// Execute.
+            /// </summary>
+            Execute,
+        }
+
+        /// <summary>
+        /// Current mode.
+        /// </summary>
+        public static Mode CurrentMode { get; internal set; } = Mode.Non;
+
+        /// <summary>
+        /// If CurrentMode is Analyze or Capture, it return true.
+        /// </summary>
+        public static bool IsCreatingMode => CurrentMode == Mode.Analyze || CurrentMode == Mode.Capture;
+    }
+}
