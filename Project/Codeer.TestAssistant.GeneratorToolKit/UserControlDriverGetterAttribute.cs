f14d7feb33c4dfa5dc2576ce13df743bbb1ceab3
codeer_ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2020-06-08 12:03:10 +0900

    Change spec, UserControlDriverGetterAttribute can assign to method.

--- Project/Codeer.TestAssistant.GeneratorToolKit/UserControlDriverGetterAttribute.cs
+++ Project/Codeer.TestAssistant.GeneratorToolKit/UserControlDriverGetterAttribute.cs
@@ -5,6 +5,12 @@ namespace Codeer.TestAssistant.GeneratorToolKit
     /// <summary>
     /// Attribute indicating that it is a property for obtaining UserControlDriver.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
-    public class UserControlDriverGetterAttribute : Attribute { }
+    [AttributeUsage(AttributeTargets.Property| AttributeTargets.Method)]
+    public class UserControlDriverGetterAttribute : Attribute 
+    {
+        /// <summary>
+        /// A property that represents the active item. You should be able to get the active item when you pass it to the method. The type must match the method argument.
+        /// </summary>
+        public string ActiveProperty { get; set; }
+    }
 }
