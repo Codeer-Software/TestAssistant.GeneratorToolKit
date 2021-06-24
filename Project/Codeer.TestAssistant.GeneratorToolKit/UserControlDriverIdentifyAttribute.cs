fccf7846723c247588543b1149958d191ad7e862
Ishikawa-Tatsuya <stone.guitar.777@gmail.com>
2021-06-24 11:41:53 +0900

    adjust comment.

--- Project/Codeer.TestAssistant.GeneratorToolKit/UserControlDriverIdentifyAttribute.cs
+++ Project/Codeer.TestAssistant.GeneratorToolKit/UserControlDriverIdentifyAttribute.cs
@@ -12,8 +12,11 @@ namespace Codeer.TestAssistant.GeneratorToolKit
         /// Name of the method that supports UserControlDriver identification when generating capture code.
         /// T must be defined in the same class.
         /// The type is next.
-        /// <para>public static UserControlDriver AttachFunction(this Driver driver, out T identifier);</para>
+        /// <para>public static UserControlDriver AttachFunction(this Driver driver, T identifier);</para>
         /// <para>public static T[] CustomFunction(Driver driver);</para>
+        /// <para></para>
+        /// <para>public static void AttachFunction(this Driver driver, T1 identifier1, T2 identifier2);</para>
+        /// <para>public static void CustomFunction(this Driver driver, out T1[] identifier1, out T2[] identifier2);</para>
         /// </summary>
         public string CustomMethod { get; set; }
     }
