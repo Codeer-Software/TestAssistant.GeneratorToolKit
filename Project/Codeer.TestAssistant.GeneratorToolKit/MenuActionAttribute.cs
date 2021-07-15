254934c382a2a4ab9717236498c8f7a1e8217ca7
uraraworks <yonehara@urara-works.jp>
2021-07-15 16:59:52 +0900

    MenuAction属性にコメント追加

--- Project/Codeer.TestAssistant.GeneratorToolKit/MenuActionAttribute.cs
+++ Project/Codeer.TestAssistant.GeneratorToolKit/MenuActionAttribute.cs
@@ -2,8 +2,14 @@
 
 namespace Codeer.TestAssistant.GeneratorToolKit
 {
+    /// <summary>
+    /// Attributes for expanding the menu
+    /// </summary>
     public class MenuActionAttribute : Attribute
     {
+        /// <summary>
+        /// Item name to be displayed in the menu
+        /// </summary>
         public string DisplayName { get; set; }
     }
 }
