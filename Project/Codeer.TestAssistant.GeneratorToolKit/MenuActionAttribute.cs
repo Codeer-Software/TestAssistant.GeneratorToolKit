eaae2d55de3b17f86a4820584726b777f400f8bb
uraraworks <yonehara@urara-works.jp>
2021-08-23 23:56:51 +0900

    MenuActionAttributeのコンストラクタに引数を追加

--- Project/Codeer.TestAssistant.GeneratorToolKit/MenuActionAttribute.cs
+++ Project/Codeer.TestAssistant.GeneratorToolKit/MenuActionAttribute.cs
@@ -11,5 +11,19 @@ namespace Codeer.TestAssistant.GeneratorToolKit
         /// Item name to be displayed in the menu
         /// </summary>
         public string DisplayName { get; set; }
+
+        /// <summary>
+        /// AccessPath of the Item
+        /// </summary>
+        public string AccessPath { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="accessPath">AccessPath of the Item</param>
+        public MenuActionAttribute(string accessPath)
+        {
+            AccessPath = accessPath;
+        }
     }
 }
